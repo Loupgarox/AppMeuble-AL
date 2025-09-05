8357c6448dda4af45fbedcfac112c301c1ab5201
arthur lelieur <arthur.lelieur2@gmail.com>
2025-09-05 10:58:12 +0200

    Petit exposé et test sur les fonctions lambda

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-import { MeModule } from "./src/meuble/MeModule";
-
-let m1 = new MeModule( 200, 100, 80 );
-
-console.log( m1 );
-
-let m2 = new MeModule();
-
-console.log( m2 );
 
 
+function compte( debut, fin, fctAffiche)
+{
+    for( let i=debut; i<= fin; i++)
+    {
+        fctAffiche( i );
+    }
+}
+
+function affiche( i )
+{
+    console.log( "la valeur de i est " + i);
+}
+
+let fct = affiche ; // Copie la réference de la fonction affiche dans la variable fct
+
+fct( 100 );
+
+compte(1, 10, affiche); // Appel de compte avec la référence de la fonction affiche
+
+
+// Appel de compte avec une fonction lambda d'affichage
+compte( 1, 10, function (valeur)
+{
+    console.log( "i possède la valeur " + valeur );
+});
+
+// Appel de compte avec une fonction lambda d'affichage autre version
+compte( 1, 10, (valeur) =>
+{
+    console.log( "i possède la valeur " + valeur );
+});
 
 
