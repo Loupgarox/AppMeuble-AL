a7d26f4d68c40dd1a99e987c7f21d8dd0206479f
arthur lelieur <arthur.lelieur2@gmail.com>
2025-09-05 10:25:56 +0200

    Ajout des contraintes sur les valeurs des attributs largeur, hauteur et profondeur de la classe MeModule et testunitaire manuel

--- src/meuble/MeModule.js
+++ src/meuble/MeModule.js
@@ -2,14 +2,27 @@
 
 export class MeModule
 {
-    constructor( largeur, hauteur=0, profondeur=0 )
+    constructor( largeur=30, hauteur=10, profondeur=30 )
     {
-        if (largeur == undefined)
-            this.largeur = 0;
+        if (largeur < 30)
+            this.largeur = 30;
+        else if (largeur > 120)
+            this.largeur = 120;
         else
             this.largeur = largeur;
 
-        this.hauteur = hauteur ;
-        this.profondeur = profondeur ;
+        if (hauteur < 30)
+            this.hauteur = 30;
+        else if (hauteur > 120)
+            this.hauteur = 120;
+        else
+            this.hauteur = hauteur;
+
+         if (profondeur < 30)
+            this.profondeur = 30;
+        else if (profondeur > 120)
+            this.profondeur = 120;
+        else
+            this.profondeur = profondeur;
     }
 }
