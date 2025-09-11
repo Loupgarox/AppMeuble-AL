8dcac3121ed63fe6013e5fe9bf580ee1b149a225
arthur lelieur <arthur.lelieur2@gmail.com>
2025-09-11 08:22:47 +0200

    wip

--- src/meuble/MeModule.js
+++ src/meuble/MeModule.js
@@ -5,11 +5,11 @@ export class MeModule
     constructor( largeur=30, hauteur=10, profondeur=30 )
     {
         if (largeur < 30)
-            this.largeur = 30;
+            this._largeur = 30;
         else if (largeur > 120)
-            this.largeur = 120;
+            this._largeur = 120;
         else
-            this.largeur = largeur;
+            this._largeur = largeur;
 
         if (hauteur < 30)
             this.hauteur = 30;
@@ -24,5 +24,16 @@ export class MeModule
             this.profondeur = 120;
         else
             this.profondeur = profondeur;
+    }
+
+    // Getter sur l'attribut _largeur
+    get largeur()
+    {
+        return this._largeur;
+    }
+
+    set largeur( valeur )
+    {
+        this._largeur = valeur ;
     }
 }
--- test/meuble/MeModule.test.js
+++ test/meuble/MeModule.test.js
@@ -5,6 +5,8 @@ describe( "MeModule test sur les valeurs d'attribut", function()
 {
    let m1 = new MeModule( 100, 350, 50 );
 
+   m1.largeur = 500;
+
    test( "Largeur doit être > à 30 et < à 120", function()
    {
         expect( m1.largeur ).toBeGreaterThanOrEqual(30);
