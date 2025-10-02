721ab2f8fe7048811403b9e43db34768cf24ae81
arthur lelieur <arthur.lelieur2@gmail.com>
2025-10-02 10:55:45 +0200

    rajout du text qui change de couleur selon le background

--- src/ihm/DrawMeModule.js
+++ src/ihm/DrawMeModule.js
@@ -29,7 +29,19 @@ export class DrawMeModule extends View
             }
 
             idDiv.innerText = this.modele.id;
+            idDiv.style.color = this.getTextContrastColor(this.modele.couleur);
         }
         return true ;
     }
+
+    getTextContrastColor(hexColor) 
+{
+    const r = parseInt(hexColor.substring(1, 3), 16);
+    const g = parseInt(hexColor.substring(3, 5), 16);
+    const b = parseInt(hexColor.substring(5, 7), 16);
+
+    const luminance = (r * 299 + g * 587 + b * 114) / 1000;
+
+    return luminance > 125 ? "black" : "white";
+}
 }
