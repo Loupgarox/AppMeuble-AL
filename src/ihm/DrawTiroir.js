839a08568fd01d9a602b2bdfc7fab3269b30838a
arthur lelieur <arthur.lelieur2@gmail.com>
2025-10-02 09:38:33 +0200

    poignee centrée et fonctionnelle

--- src/ihm/DrawTiroir.js
+++ src/ihm/DrawTiroir.js
@@ -1,31 +1,44 @@
 import { DrawMeModule } from "./DrawMeModule.js";
 
-export class DrawTiroir extends DrawMeModule
-{
-    constructor(idDraw)
-    {
+export class DrawTiroir extends DrawMeModule {
+    constructor(idDraw) {
         super(idDraw);
     }
 
-    updateView(modele)
-    {
+    updateView(modele) {
         super.updateView(modele);
 
         const draw = document.getElementById(this.idDraw);
 
-        if (draw && this.modele)
-        {
-            draw.style.backgroundColor = this.modele.couleurpoignee;
+        if (draw && this.modele) {
+            draw.style.position = "relative";
 
-            if (this.modele.poignee === "ronde") 
-            {
-                draw.style.borderRadius = "50%";
-            } 
-            else 
-            {
-                draw.style.borderRadius = "0";
+            // Créer ou récupérer la poignée
+            let poigneeDiv = draw.querySelector(".poignee-visuelle");
+            if (!poigneeDiv) {
+                poigneeDiv = document.createElement("div");
+                poigneeDiv.classList.add("poignee-visuelle");
+                draw.appendChild(poigneeDiv);
+            }
+
+            // Centrer
+            poigneeDiv.style.top = "50%";
+            poigneeDiv.style.left = "50%";
+
+            // Couleur
+            poigneeDiv.style.backgroundColor = this.modele.couleurpoignee;
+
+            // Nettoyer les anciennes classes de forme
+            poigneeDiv.classList.remove("ronde", "rectangulaire");
+
+            // Ajouter la bonne forme
+            if (this.modele.poignee === "ronde") {
+                poigneeDiv.classList.add("ronde");
+            } else {
+                poigneeDiv.classList.add("rectangulaire");
             }
         }
+
         return true;
     }
 }
